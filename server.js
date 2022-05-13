ce9ab9edab82541956f74e9d4de909d429984d69
Inayat Ullah <zerak94@gmail.com>
2022-05-13 12:00:06 +0500

    priavte chat

--- server.js
+++ server.js
@@ -13,34 +13,38 @@ const users = [];
 
 // user connects
 io.on("connection", (socket) => {
-  // when user connects first time
-  const userId = !users.length ? 1 : Date.now();
-  socket.userId = userId;
-
+  const username = socket.handshake.query["name"];
+  socket.username = username;
   users.push(socket);
-  socket.emit("user_id", userId);
-  //   console.log(`connects ${userId} | users: ${users.length}`);
+  console.log(`connected: ${username}`);
 
   // receive message
   socket.on("message", (msg) => {
+    msg = JSON.parse(msg);
+
     // send to all including sender
     // io.emit("message", msg);
-    msg = JSON.parse(msg);
 
     // send to all except sender
     // socket.broadcast.emit("message", msg);
 
-    // send to only user 1
-    const recevier = users.filter((user) => (user.socketId = 1));
+    // send to specific socket
+    const recevier = users.filter((user) => user.username === msg.receiverName);
+    if (!recevier) return;
+
     let recevierId = recevier[0].id;
     socket.broadcast.to(recevierId).emit("message", msg.text);
   });
 
   // user disconnects
   socket.on("disconnect", () => {
-    const index = users.indexOf(socket.userId);
+    let index = null;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].username === socket.username) index = i;
+    }
+
     users.splice(index, 1);
-    // console.log(`disconnects: ${socket.userId} | users: ${users.length}`);
+    console.log(`disconnected ${socket.username}`);
   });
 });
 
