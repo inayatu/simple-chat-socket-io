1a4b2599b0bf92fbe8bd3e0ad3b7eb3d36c4a1f0
Inayat Ullah <zerak94@gmail.com>
2022-05-13 10:36:05 +0500

    send message to frist user only

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 
 const io = new Server(server);
+const PORT = 3000;
 
 app.use(express.static("public"));
 
@@ -13,33 +14,36 @@ const users = [];
 // user connects
 io.on("connection", (socket) => {
   // when user connects first time
-  const userId = Date.now();
+  const userId = !users.length ? 1 : Date.now();
   socket.userId = userId;
-  users.push(userId);
+
+  users.push(socket);
   socket.emit("user_id", userId);
-  console.log(`connects ${userId} | users: ${users.length}`);
+  //   console.log(`connects ${userId} | users: ${users.length}`);
 
   // receive message
   socket.on("message", (msg) => {
     // send to all including sender
     // io.emit("message", msg);
+    msg = JSON.parse(msg);
 
     // send to all except sender
-    socket.broadcast.emit("message", msg);
+    // socket.broadcast.emit("message", msg);
 
-    // send to specific room/socket
-    // let recevierId = 1111;
-    // socket.broadcast.to(recevierId).emit("message", msg);
+    // send to only user 1
+    const recevier = users.filter((user) => (user.socketId = 1));
+    let recevierId = recevier[0].id;
+    socket.broadcast.to(recevierId).emit("message", msg.text);
   });
 
   // user disconnects
   socket.on("disconnect", () => {
     const index = users.indexOf(socket.userId);
     users.splice(index, 1);
-    console.log(`disconnects: ${socket.userId} | users: ${users.length}`);
+    // console.log(`disconnects: ${socket.userId} | users: ${users.length}`);
   });
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(PORT, () => {
+  console.log(`listening on :${PORT}`);
 });
